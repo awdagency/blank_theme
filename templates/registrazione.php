a3e76cb482ac480e6524b0be2bc42f4dc2f3889e
awdagency <43855318+awdagency@users.noreply.github.com>
2021-07-15 11:21:54 +0200

    Create registrazione.php

---
+++ templates/registrazione.php
@@ -0,0 +1,13 @@
+<?php
+/* Template Name: Registrazione */
+
+get_header();
+
+$currentID = iget_queried_object_id();
+
+// $image = wp_get_attachment_url( get_post_thumbnail_id( $currentID ), 'full' );
+?>
+
+
+<?php
+get_footer();
