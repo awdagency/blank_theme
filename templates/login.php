d0c27655bfa458c5f3e74edf48aa63a8dd14ab87
awdagency <43855318+awdagency@users.noreply.github.com>
2021-07-15 11:19:41 +0200

    Update login.php

---
+++ templates/login.php
@@ -0,0 +1,27 @@
+<?php
+/* Template Name: Login */
+
+get_header();
+
+$currentID = iget_queried_object_id();
+
+// $image = wp_get_attachment_url( get_post_thumbnail_id( $currentID ), 'full' );
+?>
+<!-- <div id="loader_login">
+    <svg x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
+        <circle class="circle" stroke-width="4" cx="50" cy="50" r="44" />
+        <circle class="dot" stroke-width="3" cx="8" cy="54" r="6" >
+        <animateTransform
+            attributeName="transform"
+            dur="2s"
+            type="rotate"
+            from="0 50 48"
+            to="360 50 52"
+            repeatCount="indefinite" />
+
+        </circle>
+    </svg>
+</div> -->
+
+<?php
+get_footer();
