bd061d12961a423bac1decc5e1755ed292273abe
awdagency <43855318+awdagency@users.noreply.github.com>
2021-07-15 11:29:37 +0200

    correzione

--- templates/registrazione.php
+++ templates/registrazione.php
@@ -3,7 +3,7 @@
 
 get_header();
 
-$currentID = iget_queried_object_id();
+$currentID = get_queried_object_id();
 
 // $image = wp_get_attachment_url( get_post_thumbnail_id( $currentID ), 'full' );
 ?>
