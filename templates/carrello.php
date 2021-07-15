0d9e235e75fe8e3121073d2f1e43762e66cf118c
awdagency <43855318+awdagency@users.noreply.github.com>
2021-07-15 11:24:16 +0200

    Create carrello.php

---
+++ templates/carrello.php
@@ -0,0 +1,15 @@
+<?php
+/* Template Name: Carrello */
+
+get_header();
+
+$currentID = iget_queried_object_id();
+
+// $image = wp_get_attachment_url( get_post_thumbnail_id( $currentID ), 'full' );
+?>
+<div id="cart">
+
+</div>
+
+<?php
+get_footer();
