45cfa105a52a54a83816c5f8537111eea9a1c0dd
awdagency <43855318+awdagency@users.noreply.github.com>
2021-07-15 12:13:29 +0200

    Create thankyou.php

---
+++ templates/thankyou.php
@@ -0,0 +1,12 @@
+<?php
+/* Template Name: Thank you */
+
+get_header();
+$current_user = wp_get_current_user();
+$key = get_query_var('orderkey');
+?>
+    <div id="thank">
+
+    </div>
+<?php
+get_footer();
